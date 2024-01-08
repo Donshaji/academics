3439201220cba20f57f8d791391d85415b3d5dd3
don <69841140+Donshaji@users.noreply.github.com>
2024-01-08 23:24:53 +0530

    Update and rename php programs to isPrime.php

---
+++ php/isPrime.php
@@ -0,0 +1,22 @@
+<?php
+    function isPrime($number){
+        if($number<=1){
+            return false;
+        }
+    
+    for($i=2;$i<=sqrt($number);$i++){
+        if($number %$i==0){
+            return false;
+        }
+    }
+    return true;
+    }
+    
+    $testNumber=17;
+    
+    if(isPrime($testNumber)){
+        echo "{$testNumber} is a prime number";
+    }else{
+        echo "{$testNumber} is Not a prime number";
+    }
+?>
